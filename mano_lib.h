77b671976a52adeaa1e673959fb47ac8826888e6
monikaaat <monika.tamelyte@mif.stud.vu.lt>
2025-02-06 19:42:26 +0200

    Sukurta struktūra studentui saugoti

---
+++ mano_lib.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+struct Stud{
+    string var, pav;
+    vector <int> nd;
+    int egz;
+}
