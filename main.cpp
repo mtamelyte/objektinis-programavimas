9026a0bea91997dd6a8ec091c0d07cf3b11d892c
monikaaat <monika.tamelyte@mif.stud.vu.lt>
2025-02-11 14:05:18 +0200

    Pridėta vardų generavimo funkcija

--- main.cpp
+++ main.cpp
@@ -1,61 +1,89 @@
 #include "mano_lib.h"
 
-void VardoIvedimas(Stud & laik){
+void VardoIvedimas(Stud &laik)
+{
     cout << "Iveskite studento varda: ";
-        cin >> laik.var;
-        cout << "Iveskite studento pavarde: ";
-        cin >> laik.pav;
+    cin >> laik.var;
+    cout << "Iveskite studento pavarde: ";
+    cin >> laik.pav;
 }
 
-void PazymiuIvedimas(Stud & laik){
+void PazymiuIvedimas(Stud &laik)
+{
     bool npaz = true;
     int paz, pas;
     cout << "Iveskite studento egzamino rezultata: ";
-        cin >> laik.egz;
-        while (npaz)
-        {
-            cout << "Iveskite studento namu darbu pazymi: ";
-            cin >> paz;
-            laik.nd.push_back(paz);
-            cout << "Ar norite ivesti dar viena pazymi?" << endl;
-            cout << "1 - taip" << endl;
-            cout << "2 - ne" << endl;
-            cin >> pas;
-            if (pas == 2)
-                npaz = false;
-        }
-}
-
-void PazGeneravimas(Stud & laik)
-{
-    srand(time(NULL));
-    laik.egz=(rand()%10)+1;
-    for(int i=0; i< rand()+1; i++)
+    cin >> laik.egz;
+    while (npaz)
     {
-        laik.nd.push_back((rand()%10)+1);
+        cout << "Iveskite studento namu darbu pazymi: ";
+        cin >> paz;
+        laik.nd.push_back(paz);
+        cout << "Ar norite ivesti dar viena pazymi?" << endl;
+        cout << "1 - taip" << endl;
+        cout << "2 - ne" << endl;
+        cin >> pas;
+        if (pas == 2)
+            npaz = false;
     }
 }
 
-void Ivedimas(vector <Stud> & s, int &p)
+void VardoGeneravimas(Stud &laik)
+{
+    string m_vardas[] = {"Monika", "Auguste", "Astrida", "Nika", "Liveta", "Juste", "Beatrice", "Egle", "Emile", "Vanesa", "Anastasija", "Gabija", "Migle", "Neda", "Teja", "Vasare"};
+    string m_pav[] = {"Tamelyte", "Tumaite", "Jablonskyte", "Bukolovaite", "Ruskute", "Vaitkeviciute", "Rokaite", "Gurklyte", "Kesaite", "Balsyte", "Fedorenko", "Kameneckaite", "Cincikaite", "Davidaviciute", "Tupinyte", "Milkovaite"};
+    string v_vardas[] = {"Adomas", "Andrius", "Arnas", "Benediktas", "Dominykas", "Emilis", "Gabrielius", "Gustavas", "Ignas", "Julius", "Justas", "Karolis", "Kristijonas", "Kristupas", "Marijus", "Martynas"};
+    string v_pav[] = {"Lukosevicius", "Rakauskas", "Matuolis", "Juozapavicius", "Pronskus", "Slabada", "Rutkauskas", "Marcinkevicius", "Volodko", "Truksinas", "Andrulevicius", "Narbutis", "Pocius", "Arifovas", "Kuprys", "Simanavicius"};
+    if (rand() % 2 != 0)
+    {
+        laik.var = v_vardas[rand() % 16];
+        laik.pav = v_pav[rand() % 16];
+    }
+    else
+    {
+        laik.var = m_vardas[rand() % 16];
+        laik.pav = m_pav[rand() % 16];
+    }
+}
+
+void PazGeneravimas(Stud &laik)
+{
+    laik.egz = (rand() % 10) + 1;
+    for (int i = 0; i < rand() + 1; i++)
+    {
+        laik.nd.push_back((rand() % 10) + 1);
+    }
+}
+
+void Ivedimas(vector<Stud> &s, int &p)
 {
     bool nstud = true;
     int pas;
     while (nstud)
     {
         Stud laik;
-        VardoIvedimas(laik);
-        if (p==1) PazymiuIvedimas(laik);
-        else if(p==2) PazGeneravimas(laik);
+        if (p == 1 || p == 2)
+            VardoIvedimas(laik);
+        if (p == 1)
+            PazymiuIvedimas(laik);
+        else if (p == 2)
+            PazGeneravimas(laik);
+        else if (p == 3)
+        {
+            VardoGeneravimas(laik);
+            PazGeneravimas(laik);
+        }
         s.push_back(laik);
         cout << "Ar norite ivesti dar viena studenta?" << endl;
         cout << "1 - taip" << endl;
         cout << "2 - ne" << endl;
         cin >> pas;
-        if(pas ==2 ) nstud=false;
+        if (pas == 2)
+            nstud = false;
     }
 }
 
-double Vidurkis(vector <int> nd)
+double Vidurkis(vector<int> nd)
 {
     double sum = 0;
     for (int i : nd)
@@ -65,16 +93,18 @@ double Vidurkis(vector <int> nd)
     return sum / nd.size();
 }
 
-double Mediana(vector <int> nd)
+double Mediana(vector<int> nd)
 {
     int dydis;
     sort(nd.begin(), nd.end());
-    dydis=nd.size();
-    if(dydis%2==0) return (nd[dydis/2]+nd[(dydis/2)-1])/2;
-    else return nd[floor(nd.size()/2)];
+    dydis = nd.size();
+    if (dydis % 2 == 0)
+        return (nd[dydis / 2] + nd[(dydis / 2) - 1]) / 2;
+    else
+        return nd[floor(nd.size() / 2)];
 }
 
-void Isvedimas(vector <Stud> s, int p, int k)
+void Isvedimas(vector<Stud> s, int p, int k)
 {
     cout << setw(12) << left << "Pavardė";
     cout << setw(16) << "Vardas";
@@ -87,7 +117,7 @@ void Isvedimas(vector <Stud> s, int p, int k)
     {
         cout << setw(15) << i.pav;
         cout << setw(12) << i.var;
-        if (k== 1)
+        if (k == 1)
             cout << setw(16) << fixed << setprecision(2) << (Vidurkis(i.nd) * 0.4) + (i.egz * 0.6) << endl;
         else if (k == 2)
             cout << setw(16) << fixed << setprecision(2) << (Mediana(i.nd) * 0.4) + (i.egz * 0.6) << endl;
@@ -96,6 +126,7 @@ void Isvedimas(vector <Stud> s, int p, int k)
 
 int main()
 {
+    srand(time(NULL));
     int k, p;
     vector<Stud> studentai;
     cout << "Ką norėtum daryti?" << endl;
@@ -104,7 +135,7 @@ int main()
     cout << "3 - Sugeneruoti pažymius, vardus ir pavardes" << endl;
     cout << "4 - Baigti darbą" << endl;
     cin >> p;
-    if (p == 1 || p==2)
+    if (p != 4)
         Ivedimas(studentai, p);
     cout << "Galutinio balo skaičiavimui norėtum naudoti: " << endl;
     cout << "1 - vidurkį" << endl;
